e34c70e82b19e88870d77528bd92728e72defc15
Ayobami6 <ayobamidele006@gmail.com>
2024-10-17 13:13:58 +0100

    implemen stack with linked list

--- python/stacks/stacks.py
+++ python/stacks/stacks.py
@@ -60,25 +60,95 @@ class StackList:
         self.data = []
 
 
-new_stack = StackList(4)
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next: Node = None
 
-new_stack.push(3)
-new_stack.push(4)
-new_stack.push(4)
-new_stack.push(4)
+
+class StackLinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def __str__(self):
+        head = self.head
+        results = []
+        while head:
+            results.append(str(head.val))
+            head = head.next
+        return "\n".join(results)
+
+    def push(self, val):
+        """Push new item to the top of the stack
+        """
+        new_node = Node(val)
+        if self.head:
+            tmp = self.head
+            self.head = new_node
+            new_node.next = tmp
+        else:
+            self.head = new_node
+        self.size += 1
+
+    def pop(self):
+        """Pop the top item out of the stack
+        """
+        if not self.head:
+            raise Exception("Stack is empty")
+        tmp = self.head
+        self.head = tmp.next
+        self.size -= 1
+        return tmp.val
+
+    def is_empty(self):
+        """ Check if stack is empty
+
+        Returns:
+            boolean: True or False
+        """
+        if not self.head:
+            return True
+        else:
+            return False
+
+    def peep(self):
+        return self.head.val
+
+    def delete_stack(self):
+        self.head = None
+
+
+# new_stack = StackList(4)
+
+# new_stack.push(3)
 # new_stack.push(4)
+# new_stack.push(4)
+# new_stack.push(4)
+# # new_stack.push(4)
 
-# print(new_stack.is_full())
-# print(new_stack.is_empty())
+# # print(new_stack.is_full())
+# # print(new_stack.is_empty())
 
-new_stack.delete_stack()
+# new_stack.delete_stack()
 
-# popped = new_stack.pop()
+# # popped = new_stack.pop()
 
-peeped = new_stack.peep()
+# peeped = new_stack.peep()
 
-print(peeped)
-# print(popped)
+# print(peeped)
+# # print(popped)
 
 
-print(new_stack.size)
+# print(new_stack.size)
+
+
+linked_stack = StackLinkedList()
+
+linked_stack.push(1)
+linked_stack.push(2)
+linked_stack.push(3)
+
+print(linked_stack.size)
+linked_stack.pop()
+print(linked_stack)
